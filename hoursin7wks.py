01ad361f7b739ee1257506fad2147f47bc20fb9c
Megan Boswell <mgboswell0@gmail.com>
2016-10-18 16:12:04 -0500

    Update hoursin7wks.py
    Added code comment explaining why I did what I did.

--- hoursin7wks.py
+++ hoursin7wks.py
@@ -5,3 +5,6 @@ day = 24 * hour
 week = 7 * day
 answer = week * 7
 print answer
+
+# So, the teacher's answer to this problem simply put print and then the math to get the answer.
+# Where's the fun in that when you've already learned about strings and variables?
